2854ba39297fd1c452b7eaa142ade49eabff988d
Marmblshko <mks316skat@gmail.com>
2024-03-10 12:15:58 +0600

    created message member, rspec validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,7 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy, inverse_of: :room
+  has_many :members, dependent: :destroy
+
   scope :public_rooms, -> {where(is_private: false)}
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 1, maximum: 250}
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_09_122121) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_121027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_members_on_room_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text "message_text"
@@ -45,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_09_122121) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "members", "rooms"
+  add_foreign_key "members", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
 end
